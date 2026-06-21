2d4cdf7c07ef65dc5c8ff01c114cc28a8ea7f38b
pranshu kumar <pranshuk1174@gmail.com>
2026-06-21 13:56:01 +0530

    Refactor Sidebar component skeleton to include dynamic sidebar items and improve navigation

--- frontend/src/layouts/Sidebar.jsx
+++ frontend/src/layouts/Sidebar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
+  const SidebarItems = [
+    {
+      label: "Dashboard",
+      path: "/dashboard",
+    },
+    {
+      label: "Tasks",
+      path: "/tasks",
+    },
+    // Add more sidebar items as needed,
+  ];
+
+  
   return (
-    <div>Sidebar</div>
-  )
+    <div>
+      <ul>
+        {SidebarItems.map((item) => (
+          <li key={item.path}>
+            <NavLink
+              to={item.path}
+              className={({ isActive }) =>
+                isActive ? "text-orange-700" : "text-gray-700"
+              }
+            >
+              {item.label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
