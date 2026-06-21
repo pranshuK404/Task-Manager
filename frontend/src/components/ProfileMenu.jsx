d9c1a3975addfd733f959bc4e8a37a5c1fa0ec63
pranshu kumar <pranshuk1174@gmail.com>
2026-06-21 17:38:45 +0530

    Refactor ProfileMenu component to handle logout functionality and improve user experience

--- frontend/src/components/ProfileMenu.jsx
+++ frontend/src/components/ProfileMenu.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import { useState } from "react";
 import { Button } from "../components/index.js";
-
+import { logoutUser } from "../services/auth/index.js";
+import { useDispatch } from "react-redux";
+import { logoutSuccess } from "../feature/authSlice.js";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
-function ProfileMenu({className=""}) {
+function ProfileMenu({ className = "" }) {
+  const [submitting, setSubmitting] = useState(false);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  async function handleLogout() {
+    setSubmitting(true);
+    try {
+      await logoutUser();
+      dispatch(logoutSuccess());
+      toast.success("Logout successful");
+      navigate("/login");
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
   return (
     <div className={className}>
       <Button onClick={() => navigate("/profile")}>Profile</Button>
       <Button onClick={() => navigate("/settings")}>Settings</Button>
-      <Button onClick={() => navigate("/logout")}>Logout</Button>
+      <Button disabled={submitting} onClick={handleLogout}>
+        Logout
+      </Button>
     </div>
   );
 }
